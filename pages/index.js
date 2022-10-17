ef15c4424d3f6cb01bac2d79cf50147b01c8fd7c
Daniel <danielhpavey@gmail.com>
2022-10-17 11:30:08 +0000

    Fix apostrophes

--- pages/index.js
+++ pages/index.js
@@ -35,13 +35,13 @@ export default function Home() {
 
 <p>How is the loss of nature and climate change affecting you? Are you feeling worried, enraged, confused, despairing or motivated to act?</p>
 
-<p>We used to meet in the bar but we are now upstairs in the board room - ask at reception as you come in. Please help support the arts centre by purchasing a coffee and/or cake at the cafe bar - you're welcome to bring these up to the room. </p>
+<p>We used to meet in the bar but we are now upstairs in the board room - ask at reception as you come in. Please help support the arts centre by purchasing a coffee and/or cake at the cafe bar - you&apos;re welcome to bring these up to the room. </p>
 
 <p>Share how you are feeling with like minded people in a friendly space. No booking required, just turn up. No cost except whatever drinks etc you would like to buy.</p>
 
 <p><strong>Outreach cafes:</strong></p>
 
-<p>We are now running extra climate cafes all over the city and would love to hear from you if you would like one. We have been to St David's Church Green Event and we are at Sylvania Community Stores and Cafe on Thursday 20th October 2022 between 10am and midday. We hope to be in the new Climate Action Hub soon too. Get in touch today. </p>
+<p>We are now running extra climate cafes all over the city and would love to hear from you if you would like one. We have been to St David&apos;s Church Green Event and we are at Sylvania Community Stores and Cafe on Thursday 20th October 2022 between 10am and midday. We hope to be in the new Climate Action Hub soon too. Get in touch today. </p>
 
 <p><strong>Facilitators:</strong></p>
 
